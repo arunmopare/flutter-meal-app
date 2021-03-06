a25c4b67c30ee808e096f0b24030241c3cbcd438
Arun Mopare <arunmopare546@gmail.com>
2021-03-06 10:51:32 +0530

    meal iteam - added more details on card

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
 import '../models/meal.dart';
 import '../data/dummy_data.dart';
 
@@ -27,7 +28,13 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability:
+                    categoryMeals[index].affordability);
           },
           itemCount: categoryMeals.length,
         ));
